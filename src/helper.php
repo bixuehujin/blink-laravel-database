2c6cde4e52c0ecd544670b63938eb7d9d2f44490
Jin Hu <bixuehujin@gmail.com>
2016-01-21 18:09:13 +0800

    Added capsule_conn and validate helper

--- src/helper.php
+++ src/helper.php
@@ -9,10 +9,33 @@ function capsule()
 }
 
 /**
+ * @param null $name
+ * @return \Illuminate\Database\Connection
+ */
+function capsule_conn($name = null)
+{
+    return app('capsule')->connection($name);
+}
+
+/**
  * @param string $name The connection name
  * @return \Illuminate\Database\Schema\Builder
  */
 function capsule_schema($name = null)
 {
     return app('capsule')->connection($name)->getSchemaBuilder();
+}
+
+/**
+ * @param $data
+ * @param $rules
+ * @return \Illuminate\Validation\Validator
+ */
+function validate($data, $rules)
+{
+    $validator = new \Illuminate\Validation\Validator(app('i18n'), $data, $rules);
+
+    $validator->setPresenceVerifier(new \Illuminate\Validation\DatabasePresenceVerifier(capsule()->getDatabaseManager()));
+
+    return $validator;
 }
